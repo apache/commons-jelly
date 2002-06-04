01f444b1a17962a776af44356ebdc3f0e401baf0
James Strachan <jstrachan@apache.org>
2002-06-04 18:36:49 +0000

    Patched the <forEach> tag to properly handle the being and step attributes to miss out items in the collection.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jelly/trunk@134916 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jelly/tags/core/ForEachTag.java
+++ src/java/org/apache/commons/jelly/tags/core/ForEachTag.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//jelly/src/java/org/apache/commons/jelly/tags/core/ForEachTag.java,v 1.10 2002/05/21 07:59:33 jstrachan Exp $
- * $Revision: 1.10 $
- * $Date: 2002/05/21 07:59:33 $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//jelly/src/java/org/apache/commons/jelly/tags/core/ForEachTag.java,v 1.11 2002/06/04 18:34:33 jstrachan Exp $
+ * $Revision: 1.11 $
+ * $Date: 2002/06/04 18:34:33 $
  *
  * ====================================================================
  *
@@ -57,7 +57,7 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  * 
- * $Id: ForEachTag.java,v 1.10 2002/05/21 07:59:33 jstrachan Exp $
+ * $Id: ForEachTag.java,v 1.11 2002/06/04 18:34:33 jstrachan Exp $
  */
 
 package org.apache.commons.jelly.tags.core;
@@ -80,7 +80,7 @@ import org.apache.commons.logging.LogFactory;
 /** A tag which performs an iteration over the results of an XPath expression
   *
   * @author <a href="mailto:jstrachan@apache.org">James Strachan</a>
-  * @version $Revision: 1.10 $
+  * @version $Revision: 1.11 $
   */
 public class ForEachTag extends TagSupport {
 
@@ -132,7 +132,12 @@ public class ForEachTag extends TagSupport {
                 log.debug("Iterating through: " + iter);
             }
 
-            for (index = begin; iter.hasNext() && index < end; index += step) {
+            // ignore the first items of the iterator
+            for (index = 0; index < begin && iter.hasNext(); index++ ) {
+                iter.next();
+            }
+            
+            while (iter.hasNext() && index < end) {
                 Object value = iter.next();
                 if (var != null) {
                     context.setVariable(var, value);
@@ -141,6 +146,15 @@ public class ForEachTag extends TagSupport {
                     context.setVariable(indexVar, new Integer(index));
                 }
                 getBody().run(context, output);
+                
+                // now we need to move to next index
+                index++;
+                for ( int i = 1; i < step; i++, index++ ) {
+                    if ( ! iter.hasNext() ) {
+                       return;
+                    }
+                    iter.next();
+                }
             }
         }
         else {
