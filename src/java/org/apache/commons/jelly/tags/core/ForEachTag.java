958cea18bd885d7bcfaf62f22120551d23025b80
James Strachan <jstrachan@apache.org>
2002-10-12 12:25:23 +0000

    patched bug that <j:forEach var="i" begin="1" end="10"> wasn't running 10 times like the JSTL spec says it should. Basically the last i="10" loop wasn't running
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jelly/trunk@135292 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jelly/tags/core/ForEachTag.java
+++ src/java/org/apache/commons/jelly/tags/core/ForEachTag.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//jelly/src/java/org/apache/commons/jelly/tags/core/ForEachTag.java,v 1.15 2002/06/26 14:50:43 jstrachan Exp $
- * $Revision: 1.15 $
- * $Date: 2002/06/26 14:50:43 $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//jelly/src/java/org/apache/commons/jelly/tags/core/ForEachTag.java,v 1.16 2002/10/12 12:25:23 jstrachan Exp $
+ * $Revision: 1.16 $
+ * $Date: 2002/10/12 12:25:23 $
  *
  * ====================================================================
  *
@@ -57,7 +57,7 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  * 
- * $Id: ForEachTag.java,v 1.15 2002/06/26 14:50:43 jstrachan Exp $
+ * $Id: ForEachTag.java,v 1.16 2002/10/12 12:25:23 jstrachan Exp $
  */
 
 package org.apache.commons.jelly.tags.core;
@@ -80,7 +80,7 @@ import org.apache.commons.logging.LogFactory;
 /** A tag which performs an iteration over the results of an XPath expression
   *
   * @author <a href="mailto:jstrachan@apache.org">James Strachan</a>
-  * @version $Revision: 1.15 $
+  * @version $Revision: 1.16 $
   */
 public class ForEachTag extends TagSupport {
 
@@ -167,7 +167,7 @@ public class ForEachTag extends TagSupport {
                     varName = indexVar;
                 }
                 
-                for (index = begin; index < end; index += step ) {
+                for (index = begin; index <= end; index += step ) {
                 
                     if (varName != null) {
                         Object value = new Integer(index);
