6f4e8fda00785c08613fe0ff31afd4115f3eda78
Morgan James Delagrange <morgand@apache.org>
2002-12-27 21:12:36 +0000

    added user classpath to windows batch script
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jelly/trunk@135455 13f79535-47bb-0310-9956-ffa450edef68

--- src/bin/jelly.bat
+++ src/bin/jelly.bat
@@ -1,4 +1,4 @@
 @echo off
 
-%JAVA_HOME%\bin\java -classpath %JELLY_HOME%\lib\forehead.jar -Dforehead.conf.file=%JELLY_HOME%\bin\forehead.conf -Dant.home=%ANT_HOME% -Djelly.home=%JELLY_HOME% -Dtools.jar=%JAVA_HOME%\lib\tools.jar %JELLY_OPTS% com.werken.forehead.Forehead %*
+%JAVA_HOME%\bin\java -classpath %CLASSPATH%;%JELLY_HOME%\lib\forehead.jar -Dforehead.conf.file=%JELLY_HOME%\bin\forehead.conf -Dant.home=%ANT_HOME% -Djelly.home=%JELLY_HOME% -Dtools.jar=%JAVA_HOME%\lib\tools.jar %JELLY_OPTS% com.werken.forehead.Forehead %*
 
