03161b891de1a02c66f20449f359a8a8eb351adf
Morgan James Delagrange <morgand@apache.org>
2003-01-02 17:32:41 +0000

    applied Mark Diggory patch for configurable forehead conf location
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jelly/trunk@135463 13f79535-47bb-0310-9956-ffa450edef68

--- src/bin/jelly.bat
+++ src/bin/jelly.bat
@@ -1,4 +1,6 @@
 @echo off
 
-%JAVA_HOME%\bin\java -classpath %CLASSPATH%;%JELLY_HOME%\lib\forehead.jar -Dforehead.conf.file=%JELLY_HOME%\bin\forehead.conf -Dant.home=%ANT_HOME% -Djelly.home=%JELLY_HOME% -Dtools.jar=%JAVA_HOME%\lib\tools.jar %JELLY_OPTS% com.werken.forehead.Forehead %*
+if "%FOREHEAD_CONF%"=="" set FOREHEAD_CONF=%JELLY_HOME%\bin\forehead.conf
+
+%JAVA_HOME%\bin\java -classpath %CLASSPATH%;%JELLY_HOME%\lib\forehead.jar -Dforehead.conf.file=%FOREHEAD_CONF% -Dant.home=%ANT_HOME% -Djelly.home=%JELLY_HOME% -Dtools.jar=%JAVA_HOME%\lib\tools.jar %JELLY_OPTS% com.werken.forehead.Forehead %*
 
