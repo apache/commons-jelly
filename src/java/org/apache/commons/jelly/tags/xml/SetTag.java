48a1036840b7a9623a1bad89df2c52ff003bc617
Robert Burrell Donkin <rdonkin@apache.org>
2003-10-09 21:21:32 +0000

    Fixed typos in apache licenses
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jelly/trunk/jelly-tags/xml@135909 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jelly/tags/xml/SetTag.java
+++ src/java/org/apache/commons/jelly/tags/xml/SetTag.java
@@ -1,7 +1,7 @@
 /*
- * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//jelly/jelly-tags/xml/src/java/org/apache/commons/jelly/tags/xml/SetTag.java,v 1.2 2003/01/26 03:45:09 morgand Exp $
- * $Revision: 1.2 $
- * $Date: 2003/01/26 03:45:09 $
+ * $Header: /home/jerenkrantz/tmp/commons/commons-convert/cvs/home/cvs/jakarta-commons//jelly/jelly-tags/xml/src/java/org/apache/commons/jelly/tags/xml/SetTag.java,v 1.3 2003/10/09 21:21:26 rdonkin Exp $
+ * $Revision: 1.3 $
+ * $Date: 2003/10/09 21:21:26 $
  *
  * ====================================================================
  *
@@ -23,11 +23,11 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
+ *    any, must include the following acknowledgement:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
+ *    Alternately, this acknowledgement may appear in the software itself,
+ *    if and wherever such third-party acknowledgements normally appear.
  *
  * 4. The names "The Jakarta Project", "Commons", and "Apache Software
  *    Foundation" must not be used to endorse or promote products derived
@@ -36,7 +36,7 @@
  *
  * 5. Products derived from this software may not be called "Apache"
  *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
+ *    permission of the Apache Software Foundation.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -57,7 +57,7 @@
  * information on the Apache Software Foundation, please see
  * <http://www.apache.org/>.
  * 
- * $Id: SetTag.java,v 1.2 2003/01/26 03:45:09 morgand Exp $
+ * $Id: SetTag.java,v 1.3 2003/10/09 21:21:26 rdonkin Exp $
  */
 package org.apache.commons.jelly.tags.xml;
 
@@ -79,7 +79,7 @@ import java.util.Collections;
 /** A tag which defines a variable from an XPath expression 
   *
   * @author <a href="mailto:jstrachan@apache.org">James Strachan</a>
-  * @version $Revision: 1.2 $
+  * @version $Revision: 1.3 $
   */
 public class SetTag extends XPathTagSupport {
 
