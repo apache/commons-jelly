eb5e7b0de659d7ca3d0621b270ebed43d3492522
Dion Gillard <dion@apache.org>
2004-09-08 04:52:34 +0000

    detab
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jelly/trunk/jelly-tags/ant@136184 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jelly/task/JellyTask.java
+++ src/java/org/apache/commons/jelly/task/JellyTask.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,8 +36,8 @@ import org.apache.tools.ant.Task;
 
 import org.xml.sax.SAXException;
 
-/** 
- * <p><code>JellyTask</code> is an Ant task which will 
+/**
+ * <p><code>JellyTask</code> is an Ant task which will
  * run a given Jelly script.
  *
  * @author <a href="mailto:jstrachan@apache.org">James Strachan</a>
@@ -57,16 +57,16 @@ public class JellyTask extends Task {
 
     /** The URL of the root context for other scripts */
     private URL rootContext;
-    
+
     /** The XML output */
     private XMLOutput xmlOutput;
 
     /** The file where output is going */
-    private File output;    
+    private File output;
 
     // Task interface
     //-------------------------------------------------------------------------
-    
+
     /**
      * Excutes the Jelly script
      */
@@ -76,7 +76,7 @@ public class JellyTask extends Task {
             if ( output != null ) {
                 log( "Sending output to: " + output );
             }
-            
+
             Script script = compileScript();
             JellyContext context = getJellyContext();
             context.setVariable( "project", getProject() );
@@ -87,35 +87,35 @@ public class JellyTask extends Task {
             throw new BuildException(e, getLocation() );
         }
     }
-    
+
     // Properties
-    //-------------------------------------------------------------------------                
-    
-    /** 
+    //-------------------------------------------------------------------------
+
+    /**
      * Sets the script URL to use as an absolute URL or a relative filename
      */
     public void setScript(String script) throws MalformedURLException {
         setUrl(resolveURL(script));
     }
-    
+
     public URL getUrl() {
         return url;
     }
-    
-    /** 
-     * Sets the script URL to use 
+
+    /**
+     * Sets the script URL to use
      */
     public void setUrl(URL url) {
         this.url = url;
     }
-    
+
     /**
      * Sets the script file to use
      */
     public void setFile(File file) throws MalformedURLException {
         setUrl( file.toURL() );
     }
-    
+
     /**
      * Sets the output to generate
      */
@@ -123,22 +123,22 @@ public class JellyTask extends Task {
         this.output = output;
         xmlOutput = XMLOutput.createXMLOutput( new FileWriter( output ) );
     }
-    
+
     public XMLOutput getXMLOutput() throws IOException {
         if (xmlOutput == null) {
             xmlOutput = XMLOutput.createXMLOutput( System.out );
         }
         return xmlOutput;
     }
-    
+
     /**
      * Sets the XMLOutput used
      */
     public void setXMLOutput(XMLOutput xmlOutput) {
         this.xmlOutput = xmlOutput;
     }
-    
-    /** 
+
+    /**
      * Gets the root context
      */
     public URL getRootContext() throws MalformedURLException {
@@ -147,14 +147,14 @@ public class JellyTask extends Task {
         }
         return rootContext;
     }
-    
-    /** 
+
+    /**
      * Sets the root context
      */
     public void setRootContext(URL rootContext) {
         this.rootContext = rootContext;
     }
-    
+
     /**
      * The context to use
      */
@@ -166,27 +166,27 @@ public class JellyTask extends Task {
             text = text.substring(0, idx + 1);
             JellyContext parentContext =  new JellyContext(getRootContext(), new URL(text));
             context = new AntJellyContext(getProject() , parentContext);
-            
+
             // register the Ant tag library
             context.registerTagLibrary( "jelly:ant", new AntTagLibrary() );
         }
         return context;
     }
-    
+
     // Implementation methods
-    //-------------------------------------------------------------------------                
-    
+    //-------------------------------------------------------------------------
+
     /**
      * Compiles the script
      */
     protected Script compileScript() throws JellyException {
         XMLParser parser = new XMLParser();
-        
+
         Script script = null;
         try {
             parser.setContext(getJellyContext());
             script = parser.parse(getUrl().toString());
-        } 
+        }
         catch (IOException e) {
             throw new JellyException(e);
         }
@@ -194,19 +194,19 @@ public class JellyTask extends Task {
             throw new JellyException(e);
         }
         script = script.compile();
-        
+
         if (log.isDebugEnabled()) {
             log.debug("Compiled script: " + getUrl());
         }
         return script;
     }
-    
-    
+
+
     /**
-     * @return the URL for the relative file name or absolute URL 
+     * @return the URL for the relative file name or absolute URL
      */
     protected URL resolveURL(String name) throws MalformedURLException {
-		File file = getProject().resolveFile(name);
+        File file = getProject().resolveFile(name);
         if (file.exists()) {
             return file.toURL();
         }
