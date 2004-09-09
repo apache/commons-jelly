3486130ca2e6fce6798b90c7a80bc54e4df17e51
Dion Gillard <dion@apache.org>
2004-09-09 04:24:02 +0000

    Style test
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jelly/trunk@136200 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jelly/CompilableTag.java
+++ src/java/org/apache/commons/jelly/CompilableTag.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,7 +19,7 @@ package org.apache.commons.jelly;
 /** <p><code>Compilable</code> represents a Tag that is compilable.</p>
   *
   * @author <a href="mailto:jstrachan@apache.org">James Strachan</a>
-  * @version $Revision: 1.10 $
+  * @version $Revision: 1.11 $
   */
 
 public interface CompilableTag {
