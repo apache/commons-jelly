5298e982fc09978d956d6fb8c2e9ecd9357e7016
Paul Libbrecht <polx@apache.org>
2005-01-28 12:36:23 +0000

    Applying Michael Schuerig's patch. jelly:xml is starting to get a really nifty fully-featured biest! paul
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jelly/trunk@148883 13f79535-47bb-0310-9956-ffa450edef68

--- jelly-tags/xml/src/java/org/apache/commons/jelly/tags/xml/SetTag.java
+++ jelly-tags/xml/src/java/org/apache/commons/jelly/tags/xml/SetTag.java
@@ -30,6 +30,7 @@ import org.jaxen.XPath;
 import org.jaxen.JaxenException;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Collections;
 
@@ -44,6 +45,12 @@ import java.util.Collections;
   */
 public class SetTag extends XPathTagSupport {
 
+    private static final int RETURN_NODE_LIST = 0;
+    private static final int RETURN_FIRST_NODE = 1;
+    private static final int RETURN_STRING_LIST = 2;
+    private static final int RETURN_DELIMITED_STRING_LIST = 3;
+    private static final int RETURN_FIRST_AS_STRING = 4;
+    
     /** The Log to which logging calls will be made. */
     private Log log = LogFactory.getLog(SetTag.class);
 
@@ -59,6 +66,8 @@ public class SetTag extends XPathTagSupport {
     private Boolean single = null;
     
     private Boolean asString = null;
+
+    private String delim = null;
 
     public SetTag() {
 
@@ -86,41 +95,113 @@ public class SetTag extends XPathTagSupport {
         catch (JaxenException e) {
             throw new JellyTagException(e);
         }
-
+        
         if (value instanceof List) {
             // sort the list if xpCmp is set.
             if (xpCmp != null && (xpCmp.getXpath() != null)) {
                 Collections.sort((List)value, xpCmp);
             }
         }
-        if (single!=null) {
-            if (single.booleanValue()==true) {
-                if(value instanceof List) {
-                    List l = (List) value;
-                    if (l.size()==0)
-                        value=null;
-                    else
-                        value=l.get(0);
-                }
-                if(asString!=null && asString.booleanValue() && value instanceof Node)
-                    value = ((Node) value).getStringValue();
-            } else { // single == false
-                if(! (value instanceof List) ) {
-                    List l = null;
-                    if (value==null) {
-                        l = new ArrayList(0);
-                    } else {
-                        l = new ArrayList(1);
-                        l.add(value);
-                    }
-                    value = l;
-                }
-            }
+
+        switch ( determineReturnType() ) {
+        case RETURN_NODE_LIST:
+            value = valueAsList(value);
+            break;
+        case RETURN_FIRST_NODE:
+            value = valueAsSingle(value);
+            break;
+        case RETURN_STRING_LIST:
+            value = nodeListToStringList(valueAsList(value));
+            break;
+        case RETURN_DELIMITED_STRING_LIST:
+            value = joinDelimitedElements(nodeListToStringList(valueAsList(value)));
+            break;
+        case RETURN_FIRST_AS_STRING:
+            value = singleValueAsString(valueAsSingle(value));
+            break;
         }
 
         //log.info( "Evaluated xpath: " + select + " as: " + value + " of type: " + value.getClass().getName() );
 
         context.setVariable(var, value);
+    }
+
+    private List valueAsList( final Object value ) {
+        if (value instanceof List) {
+            return (List)value;
+        } else {
+            if (value == null) {
+                return Collections.EMPTY_LIST;
+            } else {
+                return Collections.singletonList(value);
+            }
+        }
+    }
+
+    private Object valueAsSingle( final Object value ) {
+        if (value instanceof List) {
+            List l = (List) value;
+            if (l.isEmpty())
+                return null;
+            else
+                return l.get(0);
+        } else {
+            return value;
+        }
+    }
+    
+    private String singleValueAsString( final Object value ) {
+        if (value instanceof Node) {
+            return ((Node) value).getStringValue();
+        } else {
+            return null;
+        }
+    }
+
+    private List nodeListToStringList( final List values ) {
+        List l = new ArrayList(values.size());
+        for (Iterator it = values.iterator(); it.hasNext(); ) {
+            Object v = it.next();
+            String s = singleValueAsString(v);
+            if (s != null) {
+                l.add(s);
+            }
+        }
+        return l;
+    }
+
+    private String joinDelimitedElements( final List values ) {
+        StringBuffer sb = new StringBuffer();
+        int sz = values.size();
+        for (int i = 0; i < sz; i++) {
+            String s = (String)values.get(i); 
+            sb.append(s);
+            if (i < sz - 1)
+                sb.append(delim);
+        }
+        return sb.toString();
+    }
+
+    private int determineReturnType() {
+        int resultType;
+        if (single != null && single.booleanValue()) { // first node
+            if (asString != null && asString.booleanValue()) {
+                resultType = RETURN_FIRST_AS_STRING;
+            } else {
+                resultType = RETURN_FIRST_NODE;
+            }
+        } else { // all nodes
+            if (asString != null && asString.booleanValue()) {
+                if (delim != null) {
+                    resultType = RETURN_DELIMITED_STRING_LIST;
+                } else {
+                    resultType = RETURN_STRING_LIST;
+                }
+            } else {
+                resultType = RETURN_NODE_LIST;
+            }
+        }
+        return resultType;
     }
 
     // Properties
@@ -138,11 +219,11 @@ public class SetTag extends XPathTagSupport {
     }
 
     /** If set to true will only take the first element matching.
-        If set to false, guarantees that a list is returned.
         It then guarantees that the result is of type
         {@link org.dom4j.Node} thereby making sure that, for example,
         when an element is selected, one can directly call such methods
         as setAttribute.
+        If set to false, guarantees that a list is returned.
         */
     public void setSingle(boolean single) {
         this.single = new Boolean(single);
@@ -157,12 +238,15 @@ public class SetTag extends XPathTagSupport {
       * attribute to true.
       */
     public void setAsString(boolean asString) {
-        if(asString)
-            this.single = new Boolean(asString);
         this.asString = new Boolean(asString);
     }
 
-
+    /** If set, returns a string delimited by this delimiter.
+     */
+    public void setDelim(String delim) {
+        this.delim  = delim;
+    }
+        
     /** Sets the xpath expression to use to sort selected nodes.
      *  Ignored if single is true.
      */
