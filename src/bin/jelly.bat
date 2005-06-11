94642158dea78e5ea4b94b3e47a6f066d3fe8b50
Brett Porter <brett@apache.org>
2005-06-11 07:50:08 +0000

    distribution builder
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jelly/trunk@190093 13f79535-47bb-0310-9956-ffa450edef68

--- src/bin/jelly.bat
+++ src/bin/jelly.bat
@@ -17,5 +17,5 @@
 
 if "%FOREHEAD_CONF%"=="" set FOREHEAD_CONF=%JELLY_HOME%\bin\forehead.conf
 
-%JAVA_HOME%\bin\java -classpath "%CLASSPATH%;%JELLY_HOME%\lib\forehead-1.0-beta-5.jar" "-Dforehead.conf.file=%FOREHEAD_CONF%" "-Djelly.home=%JELLY_HOME%" "-Dtools.jar=%JAVA_HOME%\lib\tools.jar" %JELLY_OPTS% com.werken.forehead.Forehead %*
+"%JAVA_HOME%"\bin\java -classpath "%CLASSPATH%;%JELLY_HOME%\lib\forehead-1.0-beta-5.jar" "-Dforehead.conf.file=%FOREHEAD_CONF%" "-Djelly.home=%JELLY_HOME%" "-Dtools.jar=%JAVA_HOME%\lib\tools.jar" %JELLY_OPTS% com.werken.forehead.Forehead %*
 
