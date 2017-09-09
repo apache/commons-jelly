0055134c05c60572742a47d346db09b3fe6b887c
Rob Tompkins <chtompki@apache.org>
2017-09-09 21:20:53 +0000

    jelly-tags -> interaction: license-header update.
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jelly/branches/1.X@1807931 13f79535-47bb-0310-9956-ffa450edef68

--- jelly-tags/interaction/src/java/org/apache/commons/jelly/tags/interaction/InteractionTagLibrary.java
+++ jelly-tags/interaction/src/java/org/apache/commons/jelly/tags/interaction/InteractionTagLibrary.java
@@ -1,9 +1,10 @@
 /*
- * Copyright 2002,2004 The Apache Software Foundation.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
@@ -19,7 +20,7 @@ import org.apache.commons.jelly.TagLibrary;
 
 /**
   * A TagLibrary to interact with the user.
-  * @author <a href="mailto:smor@hasgard.net">Stéphane MOR</a>
+  * @author <a href="mailto:smor@hasgard.net">Stï¿½phane MOR</a>
   */
 public class InteractionTagLibrary extends TagLibrary {
 
