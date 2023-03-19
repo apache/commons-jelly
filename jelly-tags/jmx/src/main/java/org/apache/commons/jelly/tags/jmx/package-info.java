a1598915e97033decfb8f01d8803629430efa365
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:07:47 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/jmx/src/main/java/org/apache/commons/jelly/tags/jmx/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A tag library for creating beans and MBeans and registering them with a JMX Server in a similar way to the Ant tasks in Apache Commons Modeler.
+ */
+package org.apache.commons.jelly.tags.jmx;
