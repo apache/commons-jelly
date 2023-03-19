4af94c211813072514ae7cd6561cf22eaf8f3a88
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:07:53 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/jsl/src/main/java/org/apache/commons/jelly/tags/jsl/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * The Jelly Stylesheet Library (JSL).
+ * <p>
+ * The JSL tag library implements an XSLT-like declarative XML based processing engine which allows dynamic stylesheets and a free mix and match of all Jelly
+ * tags within the script.
+ * </p>
+ * <p>
+ * The JSL tag library relies on the XML tags in JSTL to do most of its work, so JSL just provides the declarative XML processing via &lt;jsl:stylesheet&gt;
+ * &lt;jsl:template&gt; and &lt;jsl:applyTemplates&gt;
+ * </p>
+ */
+package org.apache.commons.jelly.tags.jsl;
