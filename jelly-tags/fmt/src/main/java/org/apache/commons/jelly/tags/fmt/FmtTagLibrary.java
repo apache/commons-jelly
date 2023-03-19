189ebf3b18c00cabc7d6580def49617a492e1d00
Gary Gregory <garydgregory@gmail.com>
2023-03-19 10:47:39 -0400

    Fix Javadoc errors

--- jelly-tags/fmt/src/main/java/org/apache/commons/jelly/tags/fmt/FmtTagLibrary.java
+++ jelly-tags/fmt/src/main/java/org/apache/commons/jelly/tags/fmt/FmtTagLibrary.java
@@ -23,10 +23,10 @@ import org.apache.commons.jelly.TagLibrary;
  * @author <a href="mailto:willievu@yahoo.com">Willie Vu</a>
  * @version 1.2
  *
- * @task implement &lt;fmt:formatNumber&gt;
- * @task implement &lt;fmt:parseNumber&gt;
- * @task implement &lt;fmt:parseDate&gt;
- * @task decide how to support &lt;fmt:requestEncoding&gt;
+ * task implement &lt;fmt:formatNumber&gt;
+ * task implement &lt;fmt:parseNumber&gt;
+ * task implement &lt;fmt:parseDate&gt;
+ * task decide how to support &lt;fmt:requestEncoding&gt;
  */
 public class FmtTagLibrary extends TagLibrary {
 
