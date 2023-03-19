7053ab9ad5b756aa0d74c8c49f89f05c28164588
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:07:09 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/html/src/main/java/org/apache/commons/jelly/tags/html/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Tags for parsing HTML so that it can be processed by Jelly using Andy Clark's <a href="http://www.apache.org/~andyc/neko/doc/index.html">NekoHTML</a>.
+ */
+package org.apache.commons.jelly.tags.html;
---
+++ jelly-tags/http/src/main/java/org/apache/commons/jelly/tags/http/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A tag library for working with HTTP, performing HTTP GET, POST and other actions.
+ */
+package org.apache.commons.jelly.tags.http;
