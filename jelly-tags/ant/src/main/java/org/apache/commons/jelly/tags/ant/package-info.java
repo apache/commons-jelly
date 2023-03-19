e541ba1cf5df1c2a2cfeb6c75895fc30111ef715
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:05:42 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/ant/src/main/java/org/apache/commons/jelly/tags/ant/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * A tag library for using Ant tasks within Jelly.
+ * <p>
+ * Jelly can be invoked inside Ant and this tag library allows Ant tasks to be invoked from inside Jelly. This allows Jelly to be used for more 'scripting'
+ * style targets, such as parsing XML databases, working with custom java beans, doing SQL, the use of JSTL and so forth.
+ * </p>
+ */
+package org.apache.commons.jelly.tags.ant;
