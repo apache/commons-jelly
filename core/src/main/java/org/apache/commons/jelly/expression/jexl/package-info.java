58f2e58c7b76c2afce74559e6514121183137128
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:04:57 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ core/src/main/java/org/apache/commons/jelly/expression/jexl/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Expressions used to turn the tag attribute values into compiled expression objects for languages such as Jexl, XPath, Velocity, beanshell, Rhino etc.
+ */
+package org.apache.commons.jelly.expression.jexl;
