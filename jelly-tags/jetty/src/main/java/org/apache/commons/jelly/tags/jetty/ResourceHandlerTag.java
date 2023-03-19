1f7e25a1f4304fc48cb6c920f7a9b549822369c1
Gary Gregory <garydgregory@gmail.com>
2023-03-19 11:11:57 -0400

    Fix Javadoc error

--- jelly-tags/jetty/src/main/java/org/apache/commons/jelly/tags/jetty/ResourceHandlerTag.java
+++ jelly-tags/jetty/src/main/java/org/apache/commons/jelly/tags/jetty/ResourceHandlerTag.java
@@ -45,7 +45,7 @@ public class ResourceHandlerTag extends TagSupport {
      * to the parent context, setting the allowed methods if required
      *
      * @param xmlOutput where to send output
-     * @throws Exception when an error occurs
+     * @throws JellyTagException when an error occurs
      */
     public void doTag(XMLOutput xmlOutput) throws JellyTagException {
         HttpContextTag httpContext = (HttpContextTag) findAncestorWithClass(
