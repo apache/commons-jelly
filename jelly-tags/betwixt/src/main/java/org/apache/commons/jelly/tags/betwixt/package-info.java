501e17d48306e5dd901abc24979e0852392a37fa
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:06:24 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/betwixt/src/main/java/org/apache/commons/jelly/tags/betwixt/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A tag library for turning XML into beans or beans into XML using the <a href="http://commons.apache.org/jelly/libs/betwixt/tags.html">Betwixt</a> library.
+ */
+package org.apache.commons.jelly.tags.betwixt;
