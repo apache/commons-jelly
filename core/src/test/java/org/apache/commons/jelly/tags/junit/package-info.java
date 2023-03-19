5bf6088b72b38cd44d4cb4f632ae25591cfecf6e
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:05:36 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ core/src/test/java/org/apache/commons/jelly/tags/junit/package-info.java
@@ -0,0 +1,17 @@
+/**
+ * A collection of <a href="http://www.junit.org">JUnit</a> tags for performing unit tests written in Jelly script.
+ * <p>
+ * The &lt;suite&gt; tag allows a test suite to be created and then test cases can either be individually ran or the whole suite ran.
+ * </p>
+ * <p>
+ * The &lt;case&gt; tag allows a single test case to be created as part of a suite.
+ * </p>
+ * <p>
+ * The &lt;run&gt; tag can be used to run a given Test, TestCase or TestSuite
+ * </p>
+ * <p>
+ * There is an example of these tags in action <a href=
+ * "http://cvs.apache.org/viewcvs.cgi/jakarta-commons/jelly/jelly-tags/junit/src/test/org/apache/commons/jelly/tags/junit/suite.jelly?rev=HEAD&content-type=text/vnd.viewcvs-markup">here</a>
+ * </p>
+ */
+package org.apache.commons.jelly.tags.junit;
