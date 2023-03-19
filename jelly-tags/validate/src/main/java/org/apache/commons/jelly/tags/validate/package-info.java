d2f0e774b18b526b10a0f73160d82830ee616171
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:08:56 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/validate/src/main/java/org/apache/commons/jelly/tags/validate/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * A tag library for validating XML using various schema languages like DTD, XML Schema, Relax NG as well as Relax and TREX. This tag library uses the
+ * <a href="http://iso-relax.sourceforge.net/JARV/">JARV</a> API to perform the validation.
+ * <p>
+ * By default we use the <a href="http://wwws.sun.com/software/xml/developers/multischema/">MSV</a> library by Kohsuke Kawaguchi as the implementation of JARV.
+ * </p>
+ */
+package org.apache.commons.jelly.tags.validate;
