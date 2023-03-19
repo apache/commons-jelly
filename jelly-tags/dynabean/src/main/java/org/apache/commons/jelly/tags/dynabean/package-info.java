3d7897b84ddfa86a1b49e000ca2a85406025e46f
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:06:41 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/dynabean/src/main/java/org/apache/commons/jelly/tags/dynabean/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * A tag library for creating new DynaClass and DynaBean objects from the <a href="http://commons.apache.org/beanutils/">beanutils</a> library.
+ */
+package org.apache.commons.jelly.tags.dynabean;
