6d1e3d3b16eff32d8a3b6289a4c8a244c1573092
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:07:22 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/jaxme/src/main/java/org/apache/commons/jelly/tags/jaxme/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * A <a href='http://java.sun.com/xml/jaxb/'>JAXB</a> tag library using the <a href='http://ws.apache.org/jaxme'>Apache JaxMe</a> implementation.
+ * <p>
+ * Tags are provided for marshalling java objects into XML and unmarshalling XML into java objects which will work with any JAXB implementation. The generation
+ * tag generates java objects which can be marshalled and unmarshalled by JaxMe and are specific to that implementation.
+ * </p>
+ */
+package org.apache.commons.jelly.tags.jaxme;
