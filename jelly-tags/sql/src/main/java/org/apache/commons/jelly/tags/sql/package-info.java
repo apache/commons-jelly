5a5e5e25dc803aed02c2bd13b0f54a8c2280e608
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:08:23 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/sql/src/main/java/org/apache/commons/jelly/tags/sql/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * The SQL Tags from the JSTL.
+ * <p>
+ * There are some examples of these tags in action
+ * <a href="http://cvs.apache.org/viewcvs.cgi/jakarta-commons/jelly/jelly-tags/sql/src/test/org/apache/commons/jelly/tags/sql/">here</a>.
+ * </p>
+ */
+package org.apache.commons.jelly.tags.sql;
