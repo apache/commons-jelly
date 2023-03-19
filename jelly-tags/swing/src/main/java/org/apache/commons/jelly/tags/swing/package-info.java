3c34d21fdc9e29d1118bf539aec21e9acddc3fc0
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:08:38 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/swing/src/main/java/org/apache/commons/jelly/tags/swing/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * The JellySwing Library which is a Jelly Library for creating Rich User Interfaces using Swing via XML markup (a Jelly script).
+ */
+package org.apache.commons.jelly.tags.swing;
