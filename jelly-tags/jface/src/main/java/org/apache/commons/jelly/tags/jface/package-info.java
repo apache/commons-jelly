6442d540ab14ae7748de25efa2247315773eae4a
Gary Gregory <garydgregory@gmail.com>
2023-03-19 12:07:35 -0400

    Javadoc: Convert package.html to package-info.java

---
+++ jelly-tags/jface/src/main/java/org/apache/commons/jelly/tags/jface/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * The JellyJFace Library is a Jelly Library for creating Rich User Interfaces using SWT and JFace via XML markup (a Jelly script).
+ * <p>
+ * JellyJFace is an extension of JellySWT to add support for the JFace framework.
+ * </p>
+ */
+package org.apache.commons.jelly.tags.jface;
