72f3b2131078970b63f230ad39fde2851bff5f92
Gary Gregory <garydgregory@gmail.com>
2024-11-29 07:04:04 -0500

    No need for blank Javadoc lines between Javadoc @ tags

--- jelly-tags/fmt/src/main/java/org/apache/commons/jelly/tags/fmt/FormatDateTag.java
+++ jelly-tags/fmt/src/main/java/org/apache/commons/jelly/tags/fmt/FormatDateTag.java
@@ -301,7 +301,6 @@ public class FormatDateTag extends TagSupport {
      * @param style String description of formatting style for dates and times
      * @param errCode Error code to throw if given style is invalid
      * @return java.util.DateFormat constant corresponding to given style
-     *
      * @throws JellyException if the given style is invalid
      */
     public static int getStyle(String style, String errCode)
